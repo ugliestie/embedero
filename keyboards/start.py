017c99c3d061f47171f99ab34f79a3a11472f3a0
Kira Zh. <ugliestie@proton.me>
2026-07-12 19:28:45 +0700

    fix: keyboard

--- keyboards/start.py
+++ keyboards/start.py
@@ -3,6 +3,6 @@ from aiogram.utils.i18n import gettext as _
 
 def kb_start():
     inline_kb_list = [
-        [InlineKeyboardButton(text=_("tap me!"), switch_inline_query='')]
+        [InlineKeyboardButton(text=("tap me!"), switch_inline_query='')]
     ]
     return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
